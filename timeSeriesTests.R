19fc70f5b021b143624569f636dddb53abd1f808
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2016-12-06 16:37:15 +0100

    seasonal calculation test passed

--- timeSeriesTests.R
+++ timeSeriesTests.R
@@ -10,3 +10,17 @@ library(plyr)
 
 seasonal.comps <- ddply(mini, .(locale, month), summarise,
                MeanBase = mean(precip, na.rm=TRUE))
+
+#########
+# test-dat for calculating anomalies
+month <- rep(1:12,4)
+stat <- c(rep("A",24),rep("B",24))
+value <- runif(48, 0, 1)   
+
+dp <- as.data.frame(month)
+dp$stat <- stat
+dp$value <- value
+
+dp$grp.means <-with(dp, ave(value,list(month,stat), FUN=mean))
+
+dp$yr.anomaly <- with( dp, ave(value, list(month,stat), FUN=function(x) x- mean(x)))
