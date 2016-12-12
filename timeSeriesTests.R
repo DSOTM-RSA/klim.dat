2fcfd68274f3db853f4f261f063b84ff8df41ba4
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2016-12-12 16:48:28 +0100

    updated maps to anomalies

--- timeSeriesTests.R
+++ timeSeriesTests.R
@@ -1,11 +1,4 @@
 library(tidyr)
-
-mini <- merged.df
-
-mini$date.str <- as.character(mini$date.start)
-mini$month<-substr(mini$date.str, 5, 6) %>% as.double(.)
-
-
 library(plyr)
 
 seasonal.comps <- ddply(mini, .(locale, month), summarise,
